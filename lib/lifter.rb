ac29b88fbef0025a1958a8a7339d0142c73b1afd
Kevin Karma <kevinkarma55@gmail.com>
2019-09-01 21:18:08 +0000

    Automatically backed up by Learn

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -17,4 +17,16 @@ class Lifter
   def myGyms
     self.myMemberships.gym { |membership| membership.gym }
   end
+  
+  def avgLiftTotal
+    liftTotal = 0
+    totalLifter = self.allLifters.count
+    self.allLifters.each { |lifter| liftTotal += lifter.lift_toal }
+    avgLift = liftTotal.to_f/totalLifter.to_f
+  end
+  
+  def totalGymCost(lifter)
+    totalCost = 0 
+    self.myMemberships.each { |membership| membership.
+  end
 end
